66e451660d4afa9a33eb2a6d64227559fc340f02
JpPezoM <juan.pablo.pzm@gmail.com>
2022-06-04 21:30:53 -0400

    xdddd

--- apdd.py
+++ apdd.py
@@ -3,19 +3,20 @@
 def apdEstadoFinal(Transiciones,Inicial,Final,palabra):
     s = []
     s.append('R')
-    estado=[Inicial,palabra,s[0]]
+    estado=[Inicial,palabra,s]
     for i in palabra:
-        print(i)
         for j in Transiciones:
-            if estado[0]==j[0][0] and i==j[0][1]:
+            if estado[0]==j[0][0] and i==j[0][1] and estado[2][-1]==j[0][2]:
                 estado[0]=j[1][0]
                 estado[1]=estado[1][1:]
-                print(estado)
-                if(j[1][1]=='E'):
-                    s=eliminarDelStack(s)
-                else:
+                if(j[1][1]!='E'):
                     s=agregarAlStack(s,j[1][1])
-                estado[2]=s[-1]
+                else:
+                    s=eliminarDelStack(s)
+                break
+            else:
+                print("No se acepta la palabra") 
+                return
     print(s)
     if(estado[0]==Final):
         print("La palabra es aceptada")
@@ -27,7 +28,7 @@ def agregarAlStack(s,R):
         print(1) 
         return s #No agrega nada porque se mantiene intacto el stack
     else:
-        for i in range(len(R)-2): #Si el largo de la palabra es mayor a 1, se agregan letras al stack, menos la ultima
+        for i in range(len(R)-1): #Si el largo de la palabra es mayor a 1, se agregan letras al stack, menos la ultima
             s.append(R[i])
             return s
 
@@ -38,27 +39,30 @@ def eliminarDelStack(s):
 
 transi=[]
 sigue=True
-print("Ingrese las transiciones de la forma (q0,a,R)=(q1,A), donde q es el estados,a la palabra y R es el elemento del stack")
+print("Ingrese las transiciones de la forma (q0,a,R)=(q1,A), donde 'q0' y 'q1' son los estados,'a' la palabra y 'R' y 'A' elementos del stack,coinsidere que 'E' es la palabra vacia")
 while(sigue):
     tran=input("Ingrese la transicion:")
-    abs=(tran.split('='))
-    abs[0]=abs[0].strip()
-    abs[0]=abs[0][1:len(abs[0])-1].split(',')
-    abs[1]=abs[1].strip()
-    abs[1]=abs[1][1:len(abs[1])-1].split(',')
-    transi.append(abs)
-    xd=input("quiere ingresar otra transicion (N:no,Y:si):")
-    if(xd=="N" or xd=="n"):
+    if(tran==""):
         sigue=False
+    else:
+        abs=(tran.split('='))
+        abs[0]=abs[0].strip()
+        abs[0]=abs[0][1:len(abs[0])-1].split(',')
+        abs[1]=abs[1].strip()
+        abs[1]=abs[1][1:len(abs[1])-1].split(',')
+        transi.append(abs)
+    #xd=input("quiere ingresar otra transicion (N:no,Y:si):")
+    #if(xd=="N" or xd=="n"):
+        #sigue=False
+    
 
 estadoInicial=input("ingrese estado inicial:")
-estadoFinal=False
-xd=input("De que forma el APD acepta las palabras?, ingrese '1' si es por estado final o '2' si es por stack vacio:")
-if(xd=="1"):
-    estadoFinal=True
-    final=input("ingrese estado final:")
+estadoFinal=True
+#xd=input("De que forma el APD acepta las palabras?, ingrese '1' si es por estado final o '2' si es por stack vacio:")
+#if(xd=="1"):
+    #estadoFinal=True
+final=input("ingrese estado final:")
 palabraEntrada=input("Ingrese palabra de entrada:")
 
 if(estadoFinal==True):
-    print("entre")
     apdEstadoFinal(transi,estadoInicial,final,palabraEntrada)
